63d82c8ec019f9dbbd1d2b247bfc3dc9b0125d46
RaK <464758604@qq.com>
2017-10-30 09:46:57 +0800

    测试2

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
-		$uri = 'http://';
+		$uri = 'ht111tp://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
 	exit;
 ?>
-Something is wrong with the XAMPP installation :-(
+Something is wrong with the XAMPP installation :-( aaasas
